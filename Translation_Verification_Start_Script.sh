09ff856f43be2177c8f9c4c46bcf6052de930112
pennyfea <apennyfeather@ics.com>
2021-09-07 05:42:03 -0400

    Update Translation_Verification_Start_Script.sh

--- Translation_Verification_Start_Script.sh
+++ Translation_Verification_Start_Script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-LD_LIBRARY_PATH="$HOME/Qt/5.15.2/gcc_64/lib/"
-export LD_LIBRARY_PATH 
-./Translation_Verification
+HERE="$(dirname "$(readlink -f "${0}")")"
+export LD_LIBRARY_PATH=${HERE}/usr/lib/:$LD_LIBRARY_PATH
+exec "${HERE}/usr/bin/Translation_Verification" "$@"
