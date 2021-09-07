93acc981f6a55393474f18e545343f06cc72f851
pennyfea <apennyfeather@ics.com>
2021-09-07 05:58:58 -0400

    Update Translation_Verification_Start_Script.sh

--- Translation_Verification_Start_Script.sh
+++ Translation_Verification_Start_Script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 HERE="$(dirname "$(readlink -f "${0}")")"
 export LD_LIBRARY_PATH=${HERE}/usr/lib/:$LD_LIBRARY_PATH
-cd "${HERE}/usr
+cd "${HERE}/usr"
 exec "${HERE}/usr/bin/Translation_Verification" "$@"
