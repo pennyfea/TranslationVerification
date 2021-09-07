6f8a5dce076a8616d4d08f2c5933962def75c835
pennyfea <apennyfeather@ics.com>
2021-09-07 07:04:41 -0400

    Update Translation_Verification_Start_Script.sh

--- Translation_Verification_Start_Script.sh
+++ Translation_Verification_Start_Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
- DIR="$(dirname "$(readlink -f "${0}")")"
-if [ -d "$DIR"/AppDir ]; then
+ DIR="$(dirname "$(readlink -f "${0}")")/AppDir/"
+if [ -d "$DIR" ]; then
     echo "HELLO"
     HERE="$(dirname "$(readlink -f "${0}")")"
     export LD_LIBRARY_PATH=$HOME/Qt/5.15.2/gcc_64/lib/
